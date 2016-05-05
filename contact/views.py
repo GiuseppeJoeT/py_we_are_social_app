a3804989e9438282352a0c7177c5b07e24cd96e6
Giuseppe Tiberi <tiberiweb@gmail.com>
2016-05-05 19:43:53 +0100

    Added contact() Function to Contact app Views (part 2)

--- contact/views.py
+++ contact/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from .forms import ContactForm
+from django.contrib import messages
+from django.core.mail import EmailMessage
 
 
 def contact(request):
     if request.method == 'POST':
+        form = ContactForm(request.POST)
         if form.is_valid():
             email = EmailMessage()
+            email.from_email = form.cleaned_data.get('email')
+            email.to = ['tiberiweb@gmail.com']
+            email.topic = form.cleaned_data.get('topic')
+            email.body = form.form.cleaned_data.get('message')
+            our_form = form.save(commit=False)
+            our_form.save()
+            email.send()
+            messages.add_message(request, messages.SUCCESS, 'Your contact message has been sent. Thank you.')
             return render(request, 'index.html')
         else:
             render(request, 'contact/contact.html')
